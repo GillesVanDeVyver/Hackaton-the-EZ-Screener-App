d4660e898cb83cf853f89c22c88bf51f6bef5b7e
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2022-03-19 12:09:14 +0100

    new commit

--- client2/vue.config.js
+++ client2/vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
+  pwa: {
+    name: 'Company Analyser',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      // cleanupOutdatedCaches: true,
+      skipWaiting: true
+    }
+  }
 }
