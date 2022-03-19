c8403089f401cead43126db819ede0f992f4024d
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2022-03-19 15:35:37 +0100

    new commit

--- server/src/services/Scraper.js
+++ server/src/services/Scraper.js
@@ -4,7 +4,7 @@ module.exports = {
   async getCompanyInfo(companyName) {
     return new Promise((resolve, reject) => {
       if(true) {
-        const python = spawn('python', ['../../../functions.py', `"${companyName}"`]);
+        const python = spawn('python', ['../../../functions.py', `${companyName}`]);
         let response = ''
         // collect data from script
         python.stdout.on('data', function (data) {
